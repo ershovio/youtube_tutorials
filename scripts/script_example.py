ebff23ce29813dd02d490656e6f4c26f2edc37a0
aershov <info@ershov.io>
2022-02-16 21:54:07 +0300

    added script and test examples

---
+++ scripts/script_example.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def bubble_sort(a: List[int]) -> List[int]:
+    for i in range(len(a)):
+        for j in range(i + 1, len(a)):
+            if a[i] > a[j]:
+                tmp = a[i]
+                a[i] = a[j]
+                a[j] = tmp
+    return a
+
+
+if __name__ == '__main__':
+    arr = [1, 5, 8, 6]
+    sorted_arr = bubble_sort(arr)
+    print(sorted_arr)
---
+++ scripts/test_example.py
@@ -0,0 +1,19 @@
+import unittest
+
+from .script_example import bubble_sort
+
+# https://docs.python.org/3/library/unittest.html
+class TestSort(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual(bubble_sort([]), [])
+
+    def test_sort(self):
+        self.assertEqual(bubble_sort([1, 3, 2]), [1, 2, 3])
+
+    def test_single(self):
+        self.assertEqual(bubble_sort([1]), [1])
+
+
+if __name__ == '__main__':
+    unittest.main()
